a2edca3c7ca3a34463d5b9643d90939cd8aab6ef
AzizJawir <asajidiaziz0@gmail.com>
2026-04-22 18:57:31 +0800

    34

--- prob2.cpp
+++ prob2.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
 #include "queue.h"
+#include "queue.cpp"
+
+int main() {
+    int n, k;
+    std::cin >> n >> k;
+
+    if (k <= 0 || k > n) {
+        std::cout << "error" << std::endl;
+        return 0;
+    }
+
+    Queue q;
+    init(&q);
+
+    int sum = 0;
+    int x;
+
+    try {
+        int i = 0;
+        while (i < k) {
+            std::cin >> x;
+            enqueue(&q, x);
+            sum += x;
+            i++;
+        }
+
+        std::cout << sum;
+
+        while (i < n) {
+            std::cin >> x;
+
+            int depan = front(&q);
+            dequeue(&q);
+            sum -= depan;
+
+            enqueue(&q, x);
+            sum += x;
+
+            std::cout << " " << sum;
+            i++;
+        }
+        std::cout << std::endl;
+
+    } catch (const char* msg) {
+        std::cout << "error: " << msg << std::endl;
+    }
+
+    return 0;
+}
--- queue.cpp
+++ queue.cpp
@@ -1,1 +1,43 @@
 #include "queue.h"
+
+void init(Queue* q) {
+    q->front = q->data;
+    q->rear  = q->data - 1;
+}
+
+bool isEmpty(const Queue* q) {
+    return q->rear < q->front;
+}
+
+bool isFull(const Queue* q) {
+    return q->rear >= q->data + MAX - 1;
+}
+
+void enqueue(Queue* q, int value) {
+    if (isFull(q)) {
+        throw "queue penuh, cannot enqueue";
+    }
+    q->rear++;
+    *(q->rear) = value;
+}
+
+void dequeue(Queue* q) {
+    if (isEmpty(q)) {
+        throw "queue kosong, cannot dequeue";
+    }
+    q->front++;
+}
+
+int front(const Queue* q) {
+    if (isEmpty(q)) {
+        throw "queue kosong, elemen depan not found";
+    }
+    return *(q->front);
+}
+
+int back(const Queue* q) {
+    if (isEmpty(q)) {
+        throw "queue kosong, elemen belakang not found";
+    }
+    return *(q->rear);
+}
