886129094fb12b6729ba19acc2fbdc010321a774
AzizJawir <asajidiaziz0@gmail.com>
2026-04-22 16:09:56 +0800

    12

--- prob1.cpp
+++ prob1.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <string>
 #include "stack.h"
+#include "stack.cpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    Stack s;
+    init(&s);
+
+    try {
+        for (int i = 0; i < n; i++) {
+            std::string A;
+            std::cin >> A;
+
+            if (A == "+" || A == "-" || A == "*" || A == "/") {
+                int b = peek(&s); pop(&s);
+                int a = peek(&s); pop(&s);
+
+                int hasil;
+
+                if (A == "+") hasil = a + b;
+                else if (A == "-") hasil = a - b;
+                else if (A == "*") hasil = a * b;
+                else hasil = a / b;
+
+                push(&s, hasil);
+            }
+
+            else {
+                int angka = std::stoi(A);
+                push(&s, angka);
+            }
+        }
+        std::cout << peek(&s) << std::endl;
+
+    } catch (const char* msg) {
+        std::cout << "error: " << msg << std::endl;
+    }
+
+    return 0;
+}
--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,35 @@
 #include "stack.h"
 
+void init(Stack* s) {
+    s->top = s->data - 1;
+}
+
+bool isEmpty(const Stack* s) {
+    return s->top < s->data;
+}
+
+bool isFull(const Stack* s) {
+    return s->top >= s->data + MAX - 1;
+}
+
+void push(Stack* s, int value) {
+    if (isFull(s)) {
+        throw "stack penuh, tidak bisa push";
+    }
+    s->top++;
+    *(s->top) = value;
+}
+
+void pop(Stack* s) {
+    if (isEmpty(s)) {
+        throw "stack kosong, tidak bisa pop";
+    }
+    s->top--;
+}
+
+int peek(const Stack* s) {
+    if (isEmpty(s)) {
+        throw "stack kosong, tidak ada elemen";
+    }
+    return *(s->top);
+}
