3f78611861f5a4e8804fb9dbd52f5f2da06e1c31
AzizJawir <asajidiaziz0@gmail.com>
2026-04-22 19:51:21 +0800

    rev3

--- queue.cpp
+++ queue.cpp
@@ -2,23 +2,31 @@
 #include <stdexcept>
 
 void init(Queue* q) {
-    q->front = q->data;
-    q->rear  = q->data - 1;
+    q->front = nullptr;
+    q->rear  = nullptr;
 }
 
 bool isEmpty(const Queue* q) {
-    return q->rear < q->front;
+    return q->front == nullptr;
 }
 
 bool isFull(const Queue* q) {
-    return q->rear == q->data + MAX - 1;
+    if (isEmpty(q)) return false;
+    int* nextRear = q->rear + 1;
+    if (nextRear == q->data + MAX) 
+    return nextRear == q->front;
 }
 
 void enqueue(Queue* q, int value) {
     if (isFull(q)) {
         throw std::overflow_error("queue penuh, cannot enqueue");
     }
-    q->rear++;
+    if (isEmpty(q)) {
+        q->front = q->rear = q->data;
+    } else {
+        q->rear++;
+        if (q->rear == q->data + MAX) q->rear = q->data;
+    }
     *(q->rear) = value;
 }
 
@@ -26,7 +34,12 @@ void dequeue(Queue* q) {
     if (isEmpty(q)) {
         throw std::underflow_error("queue kosong, cannot dequeue");
     }
+    if (q->front == q->rear) {
+        q->front = q->rear = nullptr;
+        return;
+    }
     q->front++;
+    if (q->front == q->data + MAX) q->front = q->data;
 }
 
 int front(const Queue* q) {
